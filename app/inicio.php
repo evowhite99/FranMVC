a7546cf1b3de0f03ecd60ccd782442bf05b3f922
franngomezz16 <franngomezz16@gmail.com>
2022-09-26 16:19:25 +0200

    commit

--- app/inicio.php
+++ app/inicio.php
@@ -1,3 +1,5 @@
 <?php
+ini_set(option: 'display_errors', value: 1);
 //carga las clases iniciales
+require_once('libs/Mysqldb.php');
 require_once('libs/Application.php');
--- app/libs/Application.php
+++ app/libs/Application.php
@@ -6,8 +6,42 @@
 
 class Application
 {
+    private $url_controller = null;
+    private $url_action = null;
+    private $url_params = [];
+
+
     function __construct()
     {
-        print 'Bienvenido a mi tienda Virtual';
+        $db = Mysqldb::getInstance()->getDatabase();
+
+        $url = $this->separarUrl();
+
+        if (!$this->urlController) {
+            require_once '../app/controllers/LoginController.php';
+            $page = new LoginController();
+            $page->index();
+        } else if (file_exists('../app/controllers/' . ucfirst($this->urlController) . 'Controller.php')) {
+            $controller = ucfirst($this->urlController) . 'Controller.php';
+            require_once '../app/controllers/' . $controller . '.php';
+            $this->urlController = new $controller;
+            $this->urlController->index();
+        }
+    }
+
+    public function separarUrl()
+    {
+        if ($_SERVER['REQUEST_URI'] != '/') {
+            $url = trim($_SERVER['REQUEST_URI'], characters: '/');
+            $url = filter_var($url, filter: FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            $this->urlController = $url[0] ?? null;
+            $this->urlAction = $url[1] ?? null;
+
+            unset($url[0], $url[1]);
+
+            $this->urlParams = array_values($url);
+        }
     }
 }
