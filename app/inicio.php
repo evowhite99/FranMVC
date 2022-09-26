30815750581acbbd61f3ee649f0154a27bf3b3d5
franngomezz16 <franngomezz16@gmail.com>
2022-09-26 16:38:49 +0200

    commitphpStorm

--- app/inicio.php
+++ app/inicio.php
@@ -1,5 +1,5 @@
 <?php
 ini_set(option: 'display_errors', value: 1);
-//carga las clases iniciales
+//carga las clases iniciales2
 require_once('libs/Mysqldb.php');
 require_once('libs/Application.php');
