d741cb7b6e3e571615bb6822389a3b850a369ed2
franngomezz16 <franngomezz16@gmail.com>
2022-09-28 21:16:07 +0200

    commitphpStorm

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class LoginController extends Controller
@@ -12,26 +11,112 @@ class LoginController extends Controller
 
     public function index()
     {
-        $data = [];
+        $data = [
+            'titulo' => 'Login',
+            'menu'   => false,
+        ];
+
         $this->view('login', $data);
     }
 
-    /*public function metodoVariable()
+    public function olvido()
     {
-        if (func_num_args() > 0) {
-            for ($i = 0; $i < func_num_args(); $i++) {
-                print func_get_arg($i) . '<br>';
-            }
-        } else {
-            print 'No hay argumentos.<br>';
-        }
+        print 'Estoy en olvido';
     }
-    public function metodoFijo($arg1 = "Uno", $arg2 = "Dos", $arg3 = "Tres")
+
+    public function registro()
     {
-        print $arg1 . '<br>';
-        print $arg2 . '<br>';
-        print $arg3 . '<br>';
-    }*/
+        $errors=[];
+        $dataForm = [];
+
+        //SI estoy entrando por el post, es decir, enviando los datos del formulario
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //procesamos la informacion recibida
+            $fisrtName = $_POST['fistName'] ?? '';//si exite lo que hay en $_POST['fist_name'] pongo eso si no exite ''
+            $lastName_1 = $_POST['lastName_1'] ?? '';
+            $lastName_2 = $_POST['lastName_2'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $password1 = $_POST['password1'] ?? '';
+            $password2 = $_POST['password2'] ?? '';
+            $address = $_POST['address'] ?? '';
+            $city = $_POST['city'] ?? '';
+            $state = $_POST['state'] ?? '';
+            $postcode = $_POST['postcode'] ?? '';
+            $country = $_POST['country'] ?? '';
+
+            $dataForm = [
+                'firstName' => $fisrtName,
+                'lastName_1' => $lastName_1,
+                'lastName_2' => $lastName_2,
+                'email' => $email,
+                'password1' => $password1,
+                'password2' => $password2,
+                'address' => $address,
+                'city' => $city,
+                'state' => $state,
+                'postcode' => $postcode,
+                'country' => $country
+            ];
+
+            if($fisrtName == ''){
+                array_push($errors,'El nombre es requerido');
+            }
+            if($lastName_1 == ''){
+                array_push($errors,'El primer apellido es requerido');
+            }
+            if($lastName_2 == ''){
+                array_push($errors,'El segundo apellido es requerido');
+            }
+            if($email == ''){
+                array_push($errors,'El email es requerido');
+            }
+            if($password1 == ''){
+                array_push($errors,'La contraseña es requerida');
+            }
+            if($password2 == ''){
+                array_push($errors,'Repetir contraseña es requerido');
+            }
+            if($address == ''){
+                array_push($errors,'La direccion es requerida');
+            }
+            if($city == ''){
+                array_push($errors,'La ciudad es requerida');
+            }
+            if($state == ''){
+                array_push($errors,'La provincia es requerida');
+            }
+            if($postcode == ''){
+                array_push($errors,'El codigo postal es requerido');
+            }
+            if($country == ''){
+                array_push($errors,'El pais es requerido');
+            }
+            if($password1!=$password2){
+                array_push($errors,'Las contraseñas deben ser iguales');
+            }
+
+            if(count($errors) == 0){
+                print 'Pasamos a dar de alta al usuario en la base de datos';
+            }else{
+                //var_dump($errors);
+                $data = [
+                    'titulo' => 'Registro',
+                    'menu'   => false,
+                    'errors' => $errors,
+                    'dataform' => $dataForm
+                ];
+            }
+
+        }else{ //Si estoy entrando por el get, es decir, clickando desde el login en "nuevo usuario"
+            //Mostramos el formulario
+            $data = [
+                'titulo' => 'Registro',
+                'menu'   => false,
+            ];
+        }
 
 
+
+        $this->view('register', $data);
+    }
 }
